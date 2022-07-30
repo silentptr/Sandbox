5561645cb0a1ae352afdc0793df4cfe68d366ca3
silentptr <imquiet07@gmail.com>
2022-07-31 04:41:47 +1000

    boilerplate matrix functions

--- include/Sandbox/math.h
+++ include/Sandbox/math.h
@@ -1,33 +1,39 @@
 #ifndef SANDBOX_MATH_H_
 #define SANDBOX_MATH_H_
 
-#include <string.h>
-
+// ====================================================================================
 // matrices are stored row major
 // but need to be transformed to column major when sent to OpenGL
+// 00 01 02 03
+// 04 05 06 07
+// 08 09 10 11
+// 12 13 14 15
+// ====================================================================================
 
 typedef struct
 {
-    float components[2];
+    float data[2];
 } Vector2;
 
 typedef struct
 {
-    float components[3];
+    float data[3];
 } Vector3;
 
 typedef struct
 {
-    float components[4];
+    float data[4];
 } Vector4;
 
 typedef struct
 {
-    float mat[16];
+    float data[16];
 } Matrix4;
 
-Vector4 SB_Vector4_New(float, float, float, float);
+// ===== Matrix4 =====
 
-Matrix4 SB_Matrix4_Identity(void);
+void SB_Matrix4_SetIdentity(Matrix4*);
+void SB_Matrix4_SetOrtho(Matrix4*, float, float, float, float, float, float);
+void SB_Matrix4_GetGLArray(Matrix4*, float*);
 
 #endif
--- src/math.c
+++ src/math.c
@@ -1,17 +1,44 @@
 #include "Sandbox/math.h"
+#include <string.h>
 
-Vector4 SB_Vector4_New(float x, float y, float z, float w)
+void SB_Matrix4_SetIdentity(Matrix4* mat4)
 {
-    return (Vector4){x, y, z, w};
+    memset(mat4, 0, sizeof(Matrix4));
+    mat4->data[0] = 1.0f;
+    mat4->data[5] = 1.0f;
+    mat4->data[10] = 1.0f;
+    mat4->data[15] = 1.0f;
+    return mat4;
 }
 
-Matrix4 SB_Matrix4_Identity(void)
+void SB_Matrix4_SetOrtho(Matrix4* mat4, float left, float right, float top, float bottom, float near, float far)
 {
-    Matrix4 mat4;
-    memset(&mat4, 0, sizeof(Matrix4));
-    mat4.mat[0] = 1.0f;
-    mat4.mat[5] = 1.0f;
-    mat4.mat[10] = 1.0f;
-    mat4.mat[15] = 1.0f;
-    return mat4;
+    memset(mat4, 0, sizeof(Matrix4));
+    mat4->data[0] = 2.0f / (right - left);
+    mat4->data[3] = -((right + left) / (right - left));
+    mat4->data[5] = 2.0f / (top - bottom);
+    mat4->data[7] = -((top + bottom) / (top - bottom));
+    mat4->data[10] = -2.0f / (far - near);
+    mat4->data[11] = -((far + near) / (far - near));
+    mat4->data[15] = 1.0f;
+}
+
+void SB_Matrix4_GetGLArray(Matrix4* mat4, float* result)
+{
+    result[0] = mat4->data[0];
+    result[1] = mat4->data[4];
+    result[2] = mat4->data[8];
+    result[3] = mat4->data[12];
+    result[4] = mat4->data[1];
+    result[5] = mat4->data[5];
+    result[6] = mat4->data[9];
+    result[7] = mat4->data[13];
+    result[8] = mat4->data[2];
+    result[9] = mat4->data[6];
+    result[10] = mat4->data[10];
+    result[11] = mat4->data[14];
+    result[12] = mat4->data[3];
+    result[13] = mat4->data[7];
+    result[14] = mat4->data[11];
+    result[15] = mat4->data[15];
 }
